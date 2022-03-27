c6a07d6f8c0c756d93a68f14d00390a4386c6693
Sam <ssaldua99@gmail.com>
2022-03-26 20:34:13 -0400

    modified helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,4 @@
-print ("Hello World, my name is Sam and I love Python!")
+print("Hello World, my name is Sam and I love Python!")
+
+userName = input("Hello! Please enter your name! ")
+print("Hello", userName, "nice to meet you")
